8a4d482a476fdb5d0db2c84c031394441dcba20c
Had-com <hadas@email.com>
2025-06-22 20:56:58 +0300

    Update devices.py
    re scaling to 10

--- custom_components/tuya_ble/devices.py
+++ custom_components/tuya_ble/devices.py
@@ -300,12 +300,12 @@ devices_database: dict[str, TuyaBLECategoryInfo] = {
                 TuyaBLEProductInfo(
                     name="SGS01 Soil Sensor",
                     sensors=[
-                        TuyaBLETemperatureMapping(dp_id=5, name="Temperature", unit="°C", scale=1),
-                        TuyaBLEHumidityMapping(dp_id=15, name="Humidity", unit="%", scale=1),  
-                        TuyaBLEBatteryMapping(dp_id=3, name="Battery", unit="%", scale=1),
-                        DPMapping(dp_id=9, name="temp_unit_convert", mapping={0: "c", 1: "f"}),
-                        DPMapping(dp_id=14, name="battery_level", mapping={0: "low", 1: "medium", 2: "high"}),  
-                        DPMapping(dp_id=6, name="unused_humidity", unit="%", scale=1),  
+                            TuyaBLETemperatureMapping(dp_id=5, name="Temperature", unit="°C", scale=10),
+                            TuyaBLEHumidityMapping(dp_id=15, name="Humidity", unit="%", scale=1),
+                            TuyaBLEBatteryMapping(dp_id=3, name="Battery", unit="%", scale=1),
+                            DPMapping(dp_id=9, name="temp_unit_convert", mapping={0: "c", 1: "f"}),
+                            DPMapping(dp_id=14, name="battery_level", mapping={0: "low", 1: "medium", 2: "high"}),
+                            DPMapping(dp_id=6, name="unused_humidity", unit="%", scale=1),
                     ],
                 ),
             ),
